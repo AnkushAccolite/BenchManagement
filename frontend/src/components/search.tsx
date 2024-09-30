b81b7c8461b139555ed3000bbf9837e4637d3c81
Aroshi <aroshi.pattanaik@accolitedigital.com>
2024-09-30 11:20:50 +0530

    Fixed search component

--- frontend/src/components/search.tsx
+++ frontend/src/components/search.tsx
@@ -1,5 +1,6 @@
 import { Input } from '@/components/ui/input'
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom' // Import useLocation
 
 interface SearchProps {
   onSearch: (value: string) => void;
@@ -8,10 +9,17 @@ interface SearchProps {
 export function Search({ onSearch }: SearchProps) {
   const [searchValue, setSearchValue] = useState('')
 
+  const location = useLocation() // Get current location
+  const isTasksPage = location.pathname === '/tasks' // Check if the path is /tasks
+
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value
     setSearchValue(value)
     onSearch(value) // Pass search value to the DataTable
+  }
+
+  if (!isTasksPage) {
+    return null // Don't render anything if not on the tasks page
   }
 
   return (
